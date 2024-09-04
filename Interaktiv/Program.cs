297fbbc52e3e1ee817b1a441da72f2fc7932642a
TE23B-Nathanailidis-Kevin <kevin.nathanailidis@elev.ga.ntig.se>
2024-09-04 14:15:30 +0200

    hgjggj

--- Interaktiv/Program.cs
+++ Interaktiv/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization;
+
+Console.WriteLine("Du är i en skog");
+Console.WriteLine("De finns två vägar, höger eller vänster.");
+Console.WriteLine("Vilken väg väljer du, höger eller vänster?");
+string vag = Console.ReadLine();
+
+if (vag.ToLower() == "höger")
+{
+    Console.WriteLine("Du går höger och ser ett monster");
+    Console.WriteLine("Vad gör du?");
+    Console.WriteLine("Vill du attackera? [ja/nej]");
+    string attackera = Console.ReadLine();
+
+    if (attackera.ToLower() == "ja")
+    {
+        Console.WriteLine("Monstret äter upp dig");
+        Console.ReadLine();
+    }
+    if (attackera.ToLower() == "nej")
+    {
+        Console.WriteLine("Du går iväg, halkar och bryter nacken");
+        Console.ReadLine();
+    }
+}
+if (vag.ToLower() == "vänster")
+{
+    Console.WriteLine("Du går vänster och ser ett stup");
+    Console.WriteLine("Vill du hoppa över? [ja/nej]");
+    string hoppa = Console.ReadLine();
+
+    if (hoppa.ToLower() == "ja")
+    {
+        Console.WriteLine("du hoppar och klarar inte hoppet, så du dör av fallet");
+        Console.ReadLine();
+    }
+    if (hoppa.ToLower() == "nej")
+        Console.WriteLine("Monstret kommer och puttar ner dig");
+    Console.ReadLine();
+}
--- Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
+++ Interaktiv/obj/Debug/net8.0/Interaktiv.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac53c8bcbc4c7d8d34540d5e95d7bf4a456009d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Interaktiv")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
